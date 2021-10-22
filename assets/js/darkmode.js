c6e6e9333cc0614f42fa5855a12f86cb44a591ef
saeidtofigh <saeidfivb@gmail.com>
2021-10-22 12:16:57 +0330

    complate dark mode

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,9 +1,10 @@
-const toggle = document.querySelector('.darkmode--toggle');
+const toggleico = document.querySelector('.darkmode--toggle');
+const toggle = document.querySelector('.darkmode');
 const glass = document.querySelector('.home__glass');
 const sidebar = document.querySelector('.home__sidebar');
 const items = document.querySelectorAll('.home__sidebar--item');
 toggle.addEventListener("click", function () {
-  this.classList.toggle("fa-sun");
+  toggleico.classList.toggle("fa-sun");
   glass.classList.toggle("dark");
   sidebar.classList.toggle("dark");
   items.forEach(e=>{
